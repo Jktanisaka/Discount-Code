50faa04af7d835d9997b8d07258ab31cee72ff99
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-08-20 16:17:54 -0700

    setup

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Discount Code</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+  <div class="flex justify-center">
+    <button id="admin-button" type="button" class="button-styling">Admin</button>
+    <button id="customer-button"type="button" class="button-styling">Customer</button>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+  <script src="main.js"></script>
+</body>
+
+</html>
