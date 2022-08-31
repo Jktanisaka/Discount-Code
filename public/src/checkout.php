0fa9e4da94eb81df8a2a8df479c71a57f024cd6f
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-08-31 13:51:44 -0700

    added limit to checkout query

--- public/src/checkout.php
+++ public/src/checkout.php
@@ -5,7 +5,8 @@ $couponName = $_REQUEST['used-coupon'];
 
 $sql = "UPDATE coupon
         SET timesUsed = timesUsed + 1
-        WHERE couponName = '$couponName' AND active = TRUE";
+        WHERE couponName = '$couponName' AND active = TRUE
+        LIMIT 1";
 
 if (mysqli_query($conn, $sql)) {
   echo "Checkout Complete";
